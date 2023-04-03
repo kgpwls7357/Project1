41ab83d37ce3bea4b8600eec325e0a086da2ca77
kgpwls7357 <kgpwls7357@gmail.com>
2023-04-03 17:46:57 +0900

    커밋3

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ def add_func(n1,n2):
 def sub_func(n1,n2):
   return n1-n2
   
-    
+def gob_func(n1,n2):
+  return n1 * n2
+
 ## 전역 변수부(클래스 변수, 인스턴스 변수)
 num1, num2, result = 100,200,0
 
@@ -18,3 +20,5 @@ print(num1,'+',num2,'=',result)
 result = sub_func(num1,num2)
 print(num1,'-',num2,'=',result)
 
+result = gob_func(num1,num2)
+print(num1,'*',num2,'=',result)
